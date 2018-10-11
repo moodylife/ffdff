00ed15348548561818b0b0e58887d87e0b0f2b5e
moodylife <36269891+moodylife@users.noreply.github.com>
2018-10-11 15:20:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,17 @@
 client.on('ready', async() => {
-var server = "374597616442540034"; // ايدي السررفر
-var channel = "492097758350802944";//ايدي الروم
+var server = "499933149019766785"; 
+var channel = "499933149573283862";
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('كودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودز ')
     },305);
 })
 
 client.on('ready', async() => {
-var server = "374597616442540034"; // ايدي السررفر
-var channel = "492097758350802944";//ايدي الروم
+var server = "499933149019766785"; 
+var channel = "499933149573283862";
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('#daily')
     },86400);
 })
+client.login("NDk5NjYzNzg3MzQ4MTk3Mzc2.DqBzeA.rC18uoeCGiY3xNTeJtTNBzh2bLc");
+
